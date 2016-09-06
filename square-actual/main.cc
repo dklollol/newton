e7deefda9f0452907f7e8d4406068873479151aa
Niels G. W. Serup <ngws@metanohi.name>
2016-09-06 17:02:22 +0200

    Jeg synes at dette er pænere.

--- square-actual/main.cc
+++ square-actual/main.cc
@@ -3,43 +3,40 @@
 #include <libplayerc++/playerc++.h>
 using namespace PlayerCc;
 
-int
-main(int argc, char *argv[])
-{
-	try
-	{
-		PlayerClient robot("localhost", 6665);
-		Position2dProxy pp(&robot, 0);
+const char* HOST = "localhost";
+const int PORT = 6665;
+const int DEVICE_INDEX = 0;
 
-        for (int i = 0; i < 4; i++) {
+int main(int argc, char *argv[]) {
+  try {
+    PlayerClient robot(HOST, PORT);
+    Position2dProxy pp(&robot, DEVICE_INDEX);
 
-          // KØR FREMAD!
-          double move_speed = 0.2;
-          timespec move_sleep = {5, 0};
-          double move_turn_rate = 0.0;
-	
-          pp.SetSpeed(move_speed, DTOR(move_turn_rate));
-          nanosleep(&move_sleep, NULL);
+    for (int i = 0; i < 4; i++) {
+      // KØR FREMAD!
+      double move_speed = 0.2;
+      timespec move_sleep = {5, 0};
+      double move_turn_rate = 0.0;
 
-          // DREJ!
-          double turn_speed = 0.0;
-          timespec turn_sleep = {6, 0};
-          double turn_turn_rate = 15.0;
+      pp.SetSpeed(move_speed, DTOR(move_turn_rate));
+      nanosleep(&move_sleep, NULL);
 
-          pp.SetSpeed(turn_speed, DTOR(turn_turn_rate));
-          nanosleep(&turn_sleep, NULL);
+      // DREJ!
+      double turn_speed = 0.0;
+      timespec turn_sleep = {6, 0};
+      double turn_turn_rate = 15.0;
 
-        }
+      pp.SetSpeed(turn_speed, DTOR(turn_turn_rate));
+      nanosleep(&turn_sleep, NULL);
+    }
+    
+    // STOP!
+    timespec stop_sleep = {1, 0};
+    pp.SetSpeed(0, 0);
+    nanosleep(&stop_sleep, NULL);
 
-		// STOP!
-        timespec stop_sleep = {1, 0};
-		pp.SetSpeed(0, 0);
-		nanosleep(&stop_sleep, NULL);
-        
-	} //end try
-	catch (PlayerCc::PlayerError e)
-	{
-		std::cerr << e << std::endl;
-		return -1;
-	}
+  } catch (PlayerCc::PlayerError e) {
+    std::cerr << e << std::endl;
+    return -1;
+  }
 }

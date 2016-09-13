51c7314cef26a89ade5725163a272c7f04d97874
KristianMH <krillemh@gmail.com>
2016-09-13 13:23:39 +0200

    tweaking the square

--- square/square.cc
+++ square/square.cc
@@ -5,26 +5,43 @@ using namespace PlayerCc;
 void run_square(char* host, int port, int device_index) {
   PlayerClient robot(host, port);
   Position2dProxy pp(&robot, device_index);
+  // KØR 1 METER FREMAD!
+  double move_init = 0.2;
+  double move_turn_init = 0.0;
+  timespec move_sleep_init = {0, 100000000};
 
-  // GØR DET FØLGENDE FIRE GANGE!
-  for (int i = 0; i < 4; i++) {
-    // KØR 1 METER FREMAD!
-    double move_speed = 0.2;
-    double move_turn_rate = 0.0;
-    timespec move_sleep = {5, 0};
+  pp.SetSpeed(move_init, DTOR(move_turn_init));
+  nanosleep(&move_sleep_init, NULL);
 
-    pp.SetSpeed(move_speed, DTOR(move_turn_rate));
-    nanosleep(&move_sleep, NULL);
+  double move_speed;
+  double move_turn_rate;
+  timespec move_sleep;
+  double turn_speed;
+  double turn_turn_rate;
+  timespec turn_sleep;
 
-    // DREJ 90 GRADER!
-    double turn_speed = 0.0;
-    double turn_turn_rate = 15.0;
-    timespec turn_sleep = {6, 0};
+    
+  for (int t = 0; t < 4; t++) {
 
-    pp.SetSpeed(turn_speed, DTOR(turn_turn_rate));
-    nanosleep(&turn_sleep, NULL);
+    // GØR DET FØLGENDE FIRE GANGE!
+    for (int i = 0; i < 4; i++) {
+      // KØR 1 METER FREMAD!
+      move_speed = 0.2;
+      move_turn_rate = 0.0;
+      move_sleep = {5, 0};
+
+      pp.SetSpeed(move_speed, DTOR(move_turn_rate));
+      nanosleep(&move_sleep, NULL);
+
+      // DREJ 90 GRADER!
+      turn_speed = 0.0;
+      turn_turn_rate = 14.7;
+      turn_sleep = {6, 0};
+
+      pp.SetSpeed(turn_speed, DTOR(turn_turn_rate));
+      nanosleep(&turn_sleep, NULL);
+    }
   }
-
   // STOP!
   timespec stop_sleep = {1, 0};
   pp.SetSpeed(0, 0);

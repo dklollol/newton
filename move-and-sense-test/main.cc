ef24257cef4e6da8c690f7eb8d2a4271c5a64d10
Niels G. W. Serup <ngws@metanohi.name>
2016-09-15 16:09:56 +0200

    Tilføj testprogram hvor kørsel og sensorlæsning begge virker i Stage og på robot.

---
+++ move-and-sense-test/main.cc
@@ -0,0 +1,43 @@
+#include <libplayerc++/playerc++.h>
+#include "sleep.hpp"
+using namespace PlayerCc;
+
+void run(char* hostname, int port, int device_index) {
+  PlayerClient robot(hostname, port);
+  Position2dProxy pp(&robot, device_index);
+  IrProxy ir(&robot, device_index);
+
+  double speed = 0.2;
+  double turn = 0.0;
+
+  pp.SetSpeed(speed, DTOR(turn));
+
+  while (true) {
+    sleep(0.8);
+    robot.Read();
+    printf("Number of IR range sensors: %i\n", ir.GetCount());
+    for (uint i = 0; i < ir.GetCount(); i++) {
+      printf("IR sensor %i: %f\n", i, ir.GetRange(i));
+    }
+  }	
+}
+
+int main(int argc, char* argv[]) {
+  char* host;
+  if (argc > 1) {
+    host = argv[1];
+  }
+  else {
+    host = (char*) "localhost";
+  }
+
+  const int port = 6665;
+  const int device_index = 0;
+  try {
+    run(host, port, device_index);
+    return EXIT_SUCCESS;
+  } catch (PlayerCc::PlayerError e) {
+    std::cerr << e << std::endl;
+    return EXIT_FAILURE;
+  }
+}

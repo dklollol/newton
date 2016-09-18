5ba919f4d2d28083c749f43414926013d683fd21
Niels G. W. Serup <ngws@metanohi.name>
2016-09-18 12:30:12 +0200

    Prøv at køre sensor-validate på Stage, men fejl.  Sensor-ider passer måske ikke helt overens.

--- sensor-validate/main.cc
+++ sensor-validate/main.cc
@@ -1,47 +1,63 @@
 #include <libplayerc++/playerc++.h>
-#include <iostream>
-#include <args-debugexperiments.h>
-#include <scorpion.h>
-#include <time.h>
-#include <curses.h>
+#include <ctime>
 using namespace PlayerCc;
 
-int detections[13] = {};
+void sleep(double seconds) {
+  struct timespec spec;
+  spec.tv_sec = (time_t) seconds;
+  spec.tv_nsec = (long) ((seconds - (double) spec.tv_sec) * 10e8);
+  nanosleep(&spec, NULL);
+}
 
-bool checkSensor(int index, float threshold, IrProxy ir) {
+void set_pull_mode(PlayerClient *robot) {
+  robot->SetDataMode(PLAYER_DATAMODE_PULL);
+  robot->SetReplaceRule(true, PLAYER_MSGTYPE_DATA, -1, -1);
+}
 
-  if (ir.GetRange(index) < threshold && detections[index] < 10) {
+
+#define N_SENSORS 13
+int detections[N_SENSORS];
+
+bool checkSensor(int index, double threshold, IrProxy* ir) {
+  if (ir->GetRange(index) < threshold && detections[index] < 10) {
     detections[index] += 1;
   } else {
-    detections[index] = 0;      
+    detections[index] = 0;
   }
 
   return (detections[index] > 9);
-}
-void run_square(char* host, int port, int device_index) {
-  PlayerClient robot(host, port);
-  Position2dProxy pp(&robot, device_index);
-  IrProxy ir(&robot, gIndex);
+}
 
-  double move_speed = 0.1;
+void run(char* hostname, int port, int device_index) {
+  PlayerClient robot(hostname, port);
+  set_pull_mode(&robot);
+
+  Position2dProxy pp(&robot, device_index);
+  IrProxy ir(&robot, device_index);
+
+  for (int i = 0; i < N_SENSORS; i++) {
+    detections[i] = 0;
+  }
+
+  double move_speed = 0.2;
   double turn_speed = 10;
 
   bool collision = false;
 
   while (true) {
-    
-    //Check for near collision
+    // Check for near collision.
     for (int i = 0; i < 9; i++) {
-      if (checkSensor(i, 0.5, ir)) {
+      if (checkSensor(i, 0.5, &ir)) {
         collision = true;
       }
     }
 
-    if (collision) {      
+    if (collision) {
       pp.SetSpeed(0, DTOR(turn_speed));
     } else {
-      pp.SetSpeed(move_speed, 0);      
+      pp.SetSpeed(move_speed, 0);
     }
+    sleep(0.01);
   }
 }
 
@@ -57,7 +73,7 @@ int main(int argc, char* argv[]) {
   const int port = 6665;
   const int device_index = 0;
   try {
-    run_square(host, port, device_index);
+    run(host, port, device_index);
     return EXIT_SUCCESS;
   } catch (PlayerCc::PlayerError e) {
     std::cerr << e << std::endl;

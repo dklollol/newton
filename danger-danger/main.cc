2a2ddd8ad248490ec4b0984955affb3e7de07740
Mark Olsen <mo@telemed.dk>
2016-09-20 15:38:30 +0200

    Added ir correction

---
+++ danger-danger/main.cc
@@ -0,0 +1,64 @@
+#include <libplayerc++/playerc++.h>
+#include <ctime>
+using namespace PlayerCc;
+
+void sleep(double seconds) {
+  struct timespec spec;
+  spec.tv_sec = (time_t) seconds;
+  spec.tv_nsec = (long) ((seconds - (double) spec.tv_sec) * 10e8);
+  nanosleep(&spec, NULL);
+}
+
+void set_pull_mode(PlayerClient &robot) {
+  robot.SetDataMode(PLAYER_DATAMODE_PULL);
+  robot.SetReplaceRule(true, PLAYER_MSGTYPE_DATA, -1, -1);
+}
+
+bool check_sensor(int index, float threshold, IrProxy &ir) {
+  return (ir.GetRange(index) < threshold);
+}
+
+void run(char* host, int port, int device_index) {
+  PlayerClient robot(host, port);
+  set_pull_mode(robot);
+
+  Position2dProxy pp(&robot, device_index);
+  IrProxy ir(&robot, device_index);
+
+  double move_speed = 0.2;
+
+  pp.SetSpeed(move_speed, 0);
+  
+  bool collision = false;
+  while (!collision) {
+    robot.Read();
+
+    // Check for near collision.
+    for (int i = 0; i < 9; i++) {
+      if (check_sensor(i, 0.5, ir)) {
+        collision = true;
+		pp.SetSpeed(0, 0);
+      }
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  char* host;
+  if (argc > 1) {
+    host = argv[1];
+  }
+  else {
+    host = (char*) "localhost";
+  }
+
+  const int port = 6665;
+  const int device_index = 0;
+  try {
+    run(host, port, device_index);
+    return EXIT_SUCCESS;
+  } catch (PlayerCc::PlayerError e) {
+    std::cerr << e << std::endl;
+    return EXIT_FAILURE;
+  }
+}

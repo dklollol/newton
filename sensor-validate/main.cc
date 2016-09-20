50846396217129221420a5306e80e6f1817d4d53
Mark Olsen <mo@telemed.dk>
2016-09-20 13:32:02 +0200

    SENSOR

--- sensor-validate/main.cc
+++ sensor-validate/main.cc
@@ -6,29 +6,22 @@
 #include <curses.h>
 using namespace PlayerCc;
 
-int detections[13] = {};
-
-bool checkSensor(int index, float threshold, IrProxy ir) {
-
-  if (ir.GetRange(index) < threshold && detections[index] < 10) {
-    detections[index] += 1;
-  } else {
-    detections[index] = 0;      
-  }
-
-  return (detections[index] > 9);
+bool checkSensor(int index, float threshold, IrProxy &ir) {
+  return (ir.GetRange(index) < threshold);
 }
 void run_square(char* host, int port, int device_index) {
   PlayerClient robot(host, port);
   Position2dProxy pp(&robot, device_index);
   IrProxy ir(&robot, gIndex);
 
-  double move_speed = 0.1;
-  double turn_speed = 10;
+  double move_speed = 0.2;
+  double turn_speed = 30;
 
   bool collision = false;
 
   while (true) {
+    robot.Read();
+    collision = false;
     
     //Check for near collision
     for (int i = 0; i < 9; i++) {

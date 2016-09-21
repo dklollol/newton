c0412c5a219f5aba26560009cb2896a5fa95ed5f
Niels G. W. Serup <ngws@metanohi.name>
2016-09-21 13:11:14 +0200

    Remove cruft.

--- ir-measurements/main.cc
+++ ir-measurements/main.cc
@@ -1,29 +1,40 @@
 #include <libplayerc++/playerc++.h>
-#include "sleep.hpp"
 using namespace PlayerCc;
 
 void wait() {
   getc(stdin);
 }
 
+void sleep(double seconds) {
+  struct timespec spec;
+  spec.tv_sec = (time_t) seconds;
+  spec.tv_nsec = (long) ((seconds - (double) spec.tv_sec) * 10e8);
+  nanosleep(&spec, NULL);
+}
+
+void set_pull_mode(PlayerClient *robot) {
+  robot->SetDataMode(PLAYER_DATAMODE_PULL);
+  robot->SetReplaceRule(true, PLAYER_MSGTYPE_DATA, -1, -1);
+}
+
 void run(char* hostname, int port, int device_index) {
   PlayerClient robot(hostname, port);
+  set_pull_mode(&robot);
+
   IrProxy ir(&robot, device_index);
 
   FILE* f = fopen("measurements.txt", "w");
 
   double dist;
-  for (int i = 0; i <= 40; i += 10) {
+  for (int i = 10; i <= 60; i += 10) {
     printf("Ready to measure distance at %d centimeters?\n", i);
     wait();
     fprintf(f, "%d cm\n", i);
     for (int j = 0; j < 20; j++) {
       robot.Read();
-      //sleep(1);
       dist = ir.GetRange(2);
-      printf("Read: %lf\n", dist);
       fprintf(f, "%lf ", dist);
-      sleep(0.5);
+      sleep(0.01);
     }
     fprintf(f, "\n");
   }

84352d50d24f068065b0b5b5546e70a4fadcec51
Niels G. W. Serup <ngws@metanohi.name>
2016-09-30 11:40:10 +0200

    try this

--- green-distance/camtest.cc
+++ green-distance/camtest.cc
@@ -13,7 +13,7 @@ int main() {
   const char *WIN_RF = "Newton CAM";
   namedWindow(WIN_RF, CV_WINDOW_AUTOSIZE);
   cvMoveWindow(WIN_RF, 400, 0);
-
+  char key;
   // Prepare frame storage
   Mat frame;
 
@@ -22,21 +22,26 @@ int main() {
   Box box1;
   box1.found = false;
 
-  cam >> frame;
-  box0 = do_work(frame);
+  while ((char) cvWaitKey(4) != 32) {
+    cam >> frame;
+    box0 = do_work(frame);
+    imshow(WIN_RF, frame);
+  }
 
-  char key = (char) cvWaitKey(4);
-  key = key;
 
-  cam >> frame;
-  box1 = do_work(frame);
+  while ((char) cvWaitKey(4) != 32) {
+    cam >> frame;
+    box1 = do_work(frame);
+    imshow(WIN_RF, frame);
+  }
+
 
   // double fov = 106.0; // Niels' camera, fix this
   // double box_height = 14.5; // Fix this
   // printf("distance: %lf\n",
   //        distance_height_known((double) box.height, get_cam_height(cam),
   //                              box_height, fov));
-  double dist = 100.0;
+  double dist = 50.0;
   printf("distance: %lf\n",
          distance_two_pictures(dist,
                                (double) box0.height, (double) box1.height));

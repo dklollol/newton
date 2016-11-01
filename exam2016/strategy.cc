731d02296031a3b8df0fc4b3806473a30314c98a
Mark Olsen <mo@telemed.dk>
2016-11-01 14:20:14 +0100

    added strategy

---
+++ exam2016/strategy.cc
@@ -0,0 +1,18 @@
+void say(string text) {
+  //std::system((string("espeak '") + string(text) + string("' &")).c_str());
+  return;
+}
+
+void execute_strategy(Position2dProxy *pp, pos_t *pos, state *robot_state, object::type landmark, double measured_angle, double measured_distance) {
+  // Move the robot according to its current state.
+  switch (robot_state) {
+    case searching: {
+      puts("searching");
+      say("searching");
+      // The robot is turning to find the first landmark.
+      
+      turn(&pp, &pos, degrees_to_radians(5.0));
+      break;
+    }
+  }
+}
---
+++ exam2016/strategy.h
@@ -0,0 +1,8 @@
+enum state {searching, align, approach,
+            drive_around_landmark,
+            triangulating,
+            drive_to_center, arrived_at_center};
+            
+void say(string text);
+
+void execute_strategy(Position2dProxy *pp, pos_t *pos, state *robot_state, object::type landmark, double measured_angle, double measured_distance);

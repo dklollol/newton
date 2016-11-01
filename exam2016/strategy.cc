0938dbcc8e385decfec90c5f6bc15962f9c41474
Niels G. W. Serup <ngws@metanohi.name>
2016-11-01 22:13:50 +0100

    More general state printing

--- exam2016/strategy.cc
+++ exam2016/strategy.cc
@@ -1,18 +1,22 @@
 #include "strategy.h"
 
+char* driving_state_name[] = {"searching"};
+
 void execute_strategy(Position2dProxy &pp, pos_t &pos,
                       driving_state_t &driving_state, object::type landmark,
                       double measured_distance, double measured_angle) {
   double landmark_y;
   double landmark_x;
-
   decide_landmark(landmark, &landmark_x, &landmark_y);
+
+  char* state_name = driving_state_name[driving_state];
+  printf("[STATE] %s\n", state_name);
+  say(state_name);
+  
   // Move the robot according to its current state.
   switch (driving_state) {
     case searching: {
       // The robot is turning to find the first landmark.
-      puts("[STATE] searching");
-      say("searching");
 
       turn(pp, pos, degrees_to_radians(5.0));
       break;

7b1e16af8cb0da4a39bcee740d1e980d474e9095
KristianMH <krillemh@gmail.com>
2016-11-07 15:42:30 +0100

    bug fixing

--- exam2016/strategy.cc
+++ exam2016/strategy.cc
@@ -12,6 +12,7 @@ double stop_dist = 90;
 double angles_to_turn = 0;
 int drive_around_landmark_remaining_dist = 0;
 int square_turns = 0;
+
 void print_landmark_status() {
   printf("Seen landmark 1? %d and visited?: %d\n", seen_landmarks[object::landmark1],
          visited_landmarks[object::landmark1]);
@@ -47,10 +48,6 @@ void execute_strategy(PlayerClient &robot,
                       Position2dProxy &pp, IrProxy &ir, pos_t &pos, particle &p,
                       driving_state_t &driving_state, object::type landmark,
                       double measured_distance, double measured_angle) {
-  // double landmark_y;
-  // double landmark_x;
-  // decide_landmark(landmark, &landmark_x, &landmark_y);
-
   string state_name = stateMap[driving_state];
   printf("[STATE] %s\n", state_name.c_str());
   // say_async(state_name);
@@ -68,7 +65,7 @@ void execute_strategy(PlayerClient &robot,
     printf("We should turn : %f degress\n", radians_to_degrees(angle));
     printf("we should drive : %f cm\n", dist);
     turn(pp, pos, angle);
-    drive(robot, pp, ir, pos, dist);
+    drive(robot, pp, ir, pos, dist, true);
     
     visited_landmarks[landmark] = true;
     if (n_landmark == object::landmark4) {
@@ -106,12 +103,12 @@ void execute_strategy(PlayerClient &robot,
       drive_around_landmark_remaining_dist = 50;
       GOTO(searching_sqaure);
       square_turns = 0;
-      angles_to_turn = 90;
+      angles_to_turn = -90;
       visited_landmarks[landmark] = true;
       print_landmark_status();
     } else {
       drive(robot, pp, ir, pos,
-            clamp(measured_distance - stop_dist, 0.0, 15.0));
+            clamp(measured_distance - stop_dist, 0.0, 15.0), false);
       GOTO(driving_state_t::align); // Make sure it's still aligned.
     }
     break;
@@ -127,29 +124,15 @@ void execute_strategy(PlayerClient &robot,
       }
     }
     if (!driven) {
-      drive(robot, pp, ir, pos, 50);
+      drive(robot, pp, ir, pos, 50, true);
       driven = true;
       break;
     }
     if (angles_to_turn == 0) {
       angles_to_turn = randf() > 0.5 ? randf()*180 : -1*randf()*180;
     }
-    /*
-    if (angles_to_turn < 0) {
-      turn(pp, pos, degrees_to_radians(-5));
-      angles_to_turn += 5;
-    } else {
-      turn(pp, pos, degrees_to_radians(5));
-      angles_to_turn -=5;
-    }
-
-    if (abs(angles_to_turn) < 5) {
-      driven = false;
-      angles_to_turn = 0;
-    }
-    */
-
     if (handle_turning(pp, pos, angles_to_turn, degrees_to_radians(5))) {
+      angles_to_turn = 0;
       driven = false;
     }
     break;
@@ -166,23 +149,28 @@ void execute_strategy(PlayerClient &robot,
     }
     if (!driven) {
       driven = true;
-      drive(robot, pp, ir, pos, drive_around_landmark_remaining_dist);
+      drive(robot, pp, ir, pos, drive_around_landmark_remaining_dist, false);
       drive_around_landmark_remaining_dist = 110;
       break;
     }
     
-    if (angles_to_turn == 0) {
-      angles_to_turn = 90;
+    // if (angles_to_turn == 0) {
+    //   angles_to_turn = -90;
+    //   // done turned and should drive next time
+    //   driven = false;
+    //   square_turns++;
+    // }
+    
+    // angles_to_turn -= 5;
+    // turn(pp, pos, degrees_to_radians(-5));
+    
+
+    if (handle_turning(pp, pos, angles_to_turn, degrees_to_radians(5))) {
+      angles_to_turn = -90;
       // done turned and should drive next time
       driven = false;
       square_turns++;
     }
-    /*
-    angles_to_turn -= 5;
-    turn(pp, pos, degrees_to_radians(-5));
-    */
-
-    handle_turning(pp, pos, angles_to_turn, degrees_to_radians(5));
 
     if (square_turns == 4) {
       GOTO(searching_random);

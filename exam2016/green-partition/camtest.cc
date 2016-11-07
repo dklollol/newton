1bcd0d81cf4453c6432a33d67dacf85ba845fba5
Mark <victor_olsen2@msn.com>
2016-11-07 15:05:46 +0000

    testing

--- exam2016/green-partition/camtest.cc
+++ exam2016/green-partition/camtest.cc
@@ -21,11 +21,40 @@ int main() {
 
   // Prepare frame storage.
   Mat frame;
+  Mat frame2;
 
   while (true) {
     cvWaitKey(1);
     cam >> frame;
-    imshow(WIN_RF, filter_green(frame));
+
+  frame2 = filter_green(frame);
+
+  std::vector<Box> boxes = process_vertical_lines(frame2);
+
+  size_t n_channels = frame.channels();
+  size_t n_rows = frame.rows;
+  size_t n_cols = frame.cols * n_channels;
+
+  size_t x, y;
+
+  size_t blue_index, green_index, red_index;
+  uint8_t blue, green, red;
+
+  uint8_t* pixels = frame.ptr<uint8_t>(0);
+
+  for (size_t i = 0; i < boxes.size(); i++) {
+    for (x = boxes[i].start; x <= boxes[i].end; x += n_channels) {
+      for(y = 0; y < n_rows; y++) {
+        blue_index = y * n_cols + x;
+        pixels[blue_index] = 255;
+        pixels[blue_index + 1] = 255;
+        pixels[blue_index + 2] = 255;
+      }
+    }
+  }
+
+
+    imshow(WIN_RF, frame);
   }
 
   return EXIT_SUCCESS;
